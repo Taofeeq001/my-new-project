b65c362564d3e996e2e2e2fa78ee00b004d9efa3
Taofeeq001 <125363229+Taofeeq001@users.noreply.github.com>
2023-03-23 09:33:19 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+var tgg = document.querySelector('.toggle');
+tgg.addEventListener('click', () => {
+    var tgg = document.querySelector('.toggle');
+    tgg.classList.toggle('switch')
+    var mobile = document.querySelector('.mobile-nav');
+    mobile.classList.toggle('switcher')
+})
